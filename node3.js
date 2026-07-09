92c54f7765f50fd283d930c9977af0cde832e400
shiv578 <swarajrkfl22@gmail.com>
2026-07-09 09:56:08 +0530

    learniing the file system

--- node3.js
+++ node3.js
@@ -4,17 +4,21 @@ import * as fs from 'fs';
 //     console.log("Error making the file");
 // })
 //creating the folder in node js
+
+
+
+
 // rmdir("Trial" , (error)=>{
 //     console.log("Error making the file");
-// })//
+// }) 
 
-
-//deleting the folder in node js using rmdir
+//deleting the folder in node js using rmdir but rmdir will not delter the 
+//sub folder from the folder it will just delter the empty folder
 
 
 // fs.rename('log.txt' , "password.txt" ,(error)=>{
 //     console.log("Error",error);
-// })  this is used to rename the file
+// })  this is used to rename the file 
 
 if(fs.existsSync('server.js')){
     console.log("file found");
