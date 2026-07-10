2dba7deef2eb7432aa1fe31db6771fa876f495a4
shiv578 <swarajrkfl22@gmail.com>
2026-07-10 10:53:18 +0530

    learning express

---
+++ server4.js
@@ -0,0 +1,24 @@
+//working on the expresss js
+import express from 'express';
+
+const express =  require
+
+app.get("/", (res , req)=>{
+res.send("Hellp from the server");
+
+});
+
+app.post("/" , (req,req)=>{
+    res.send("hellp form the client");
+});
+
+app.put("/" , (res,req)=>{
+    res.send("updating value in the server");
+})
+
+app.patch("/" , (req,res)=>{
+    res.send("using patch");
+})
+
+
+//rest api   it is the way to communicate over the interesr usinf the http method 
