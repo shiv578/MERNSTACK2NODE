2f83204ced75ae44772b4ae47c90b9f006269b52
shiv578 <swarajrkfl22@gmail.com>
2026-07-13 10:59:14 +0530

    learning express router

---
+++ router.js
@@ -0,0 +1,18 @@
+import express from 'express'
+
+const router =  express.Router();
+
+router.get('/Products' , (req, res)=>{
+    res.send(`products Page ${req.method}`);
+})
+.post('/Products' ,(req,res)=>{
+    res.send(`Products page ${req.method}`);
+})
+
+.post('/Products' ,(req,res)=>{
+    res.send(`Products page ${req.method}`);
+})
+
+.post('/Products' ,(req,res)=>{
+    res.send(`Products page ${req.method}`);
+})
