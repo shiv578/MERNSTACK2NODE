76493ad2a26550b978365a1fe8b6993db348d469
shiv578 <swarajrkfl22@gmail.com>
2026-07-13 09:26:28 +0530

    learning express router

---
+++ server5.js
@@ -0,0 +1,12 @@
+import express from 'express';
+ const PORT = 3000;
+
+ const app = express();
+
+ app.get('/' , (req,res)=>{
+    res.send("Index Page");
+ });
+
+ app.listen(PORT , '127.0.0.1', ()=>{
+    console.log(`node is running on local host: ${PORT}`);
+ })
